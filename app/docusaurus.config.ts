2df858636c6a9f799bd0fb4d693243e10c0eea3e
Alexis Sánchez <alexis@qtechgames.com>
2025-02-15 20:52:15 +0100

    Update supported locales in docusaurus.config.ts

--- app/docusaurus.config.ts
+++ app/docusaurus.config.ts
@@ -28,7 +28,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh', 'fr', 'es', 'hi']
+    locales: ['en', 'zh']
   },
 
   presets: [
